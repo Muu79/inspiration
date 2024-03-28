2da31bf34cf2f914d9513e44b3c51f2d1ce32726
muu <muu794@gmail.com>
2024-03-28 03:21:39 +0200

    added state persistance

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import { useEffect } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import TaskContainer from './components/TaskContainer/TaskContainer';
+import { store } from './app/store';
 
 function App() {
+  useEffect(() => {
+    const localUpdater = store.subscribe(() => {
+      const state = store.getState();
+      localStorage.setItem('store', JSON.stringify(state));
+      console.log('store updated',JSON.parse(localStorage.getItem('store') || '{}'));
+    });
+    return () => localUpdater();
+  }, [])
   return (
     <>
       <div id='background'></div>
